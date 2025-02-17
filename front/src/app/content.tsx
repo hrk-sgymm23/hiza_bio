09c192c6eb858039dbc26a290e9c4ef240b103cf
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2025-02-17 22:00:19 +0900

    feat: add image

--- front/src/app/content.tsx
+++ front/src/app/content.tsx
@@ -6,9 +6,9 @@ const Content = async () => {
     <>
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-                <Image src='/images/introduction.png' alt="Introduction" className="sm:w-32 sm:h-32 rounded-full object-cover" width={200} height={200} />
+          <div className="flex items-center lg:w-3/5 mx-auto border-b pb-12 mb-12 border-gray-200 sm:flex-row flex-col">
+            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-20 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                <Image src='/images/introduction.png' alt="Introduction" className="shadow-2xl sm:w-32 sm:h-32 rounded object-cover" width={200} height={200} />
             </div>
             <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
               <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Introduction(自己紹介)</h2>
@@ -30,13 +30,13 @@ const Content = async () => {
                 </svg>
               </a>
             </div>
-            <div className="sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-              <Image src='/images/biography.png' alt="BioGraphy" className="sm:w-32 sm:h-32 rounded-full object-cover" width={200} height={200} />
+            <div className="sm:w-32 sm:order-none order-first sm:h-32 h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full flex-shrink-0">
+              <Image src='/images/biography.png' alt="BioGraphy" className="shadow-2xl sm:w-32 sm:h-32 rounded object-cover" width={200} height={200} />
             </div>
           </div>
           <div className="flex items-center lg:w-3/5 mx-auto sm:flex-row flex-col">
-            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
-              <Image src='/images/blog.png' alt="Blog" className="sm:w-32 sm:h-32 rounded-full object-cover" width={200} height={200} />
+            <div className="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full flex-shrink-0">
+              <Image src='/images/blog.png' alt="Blog" className="shadow-2xl sm:w-32 sm:h-32 rounded object-cover" width={200} height={200} />
             </div>
             <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
               <h2 className="text-gray-900 text-lg title-font font-medium mb-2">Blog(日記)</h2>
