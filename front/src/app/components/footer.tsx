cfa16d949703ad2c77708a280ec23d7e64fb86b1
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2025-02-22 21:31:19 +0900

    feat: add biography

--- front/src/app/components/footer.tsx
+++ front/src/app/components/footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <>
       <footer className="text-gray-600 body-font">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-                <span className="ml-3 text-xl">HIZA</span>
-            </a>
+            <Link href="/">
+              <span className="ml-3 text-xl">HIZA</span>
+            </Link>
             <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2025 HIZA —
                 <a href="https://x.com/wvvvn6j" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@hrk-sgymm23</a>
             </p>
