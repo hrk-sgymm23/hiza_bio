9652093f1d589fbb7e09a48eac88e54f3e6f88df
hrk-sgymm23 <b1805473@planet.kanazawa-it.ac.jp>
2025-02-24 16:35:47 +0900

    chore: fix sns link

--- front/src/app/components/footer.tsx
+++ front/src/app/components/footer.tsx
@@ -10,11 +10,11 @@ const Footer = () => {
               <span className="ml-3 text-xl">HIZA</span>
             </Link>
             <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2025 HIZA —
-                <a href="https://x.com/wvvvn6j" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@hrk-sgymm23</a>
+                <a href="https://github.com/hrk-sgymm23" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@hrk-sgymm23</a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                 {/* FaceBook */}
-                <a href="https://x.com/wvvvn6j" className="text-gray-500">
+                <a href="https://www.facebook.com/profile.php?id=100089258139480" className="text-gray-500">
                     <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                     </svg>
@@ -26,7 +26,7 @@ const Footer = () => {
                     </svg>
                 </a>
                 {/* Instagram */}
-                <a href="https://x.com/wvvvn6j" className="ml-3 text-gray-500">
+                <a href="https://www.instagram.com/hrk_sgymmm/" className="ml-3 text-gray-500">
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                     <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                     <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
